dcf1b4ee3d11580526e993be53e41dd349df97b6
Mohamed <mohamedbanhawi@gmail.com>
2018-05-29 23:09:10 +0930

    demo perspective transform

--- Term1/Advanced Lane Finding Project 4/CarND-Advanced-Lane-Lines/lane_finding.py
+++ Term1/Advanced Lane Finding Project 4/CarND-Advanced-Lane-Lines/lane_finding.py
@@ -1,15 +1,24 @@
+# libraries 
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 import os
 import time
 
+# constants
+# enables plotting and 
 DEBUG = True
+CALIBRATION_FOLDER = 'camera_cal'
+TEST_FOLDER = 'test_images'
 
+
+# lane finding class for correcting and processing images
 class lane_finding():
     def __init__(self, show_images = False):
         # display images 
         self.show_images = show_images
+        # image size
+        self.im_size = None
         # camera calibration parameters
         self.dist = None
         self.mtx = None
@@ -20,7 +29,9 @@ class lane_finding():
         
         # Color thresholds
         
-
+        # image perspective transform
+        self.Minv = None
+        
     def process_image(self, img):
 
         # find corners in 
@@ -75,58 +86,92 @@ class lane_finding():
         ax2.set_title('Combined S channel and gradient thresholds')
         ax2.imshow(combined_binary, cmap='gray')
 
-        #Perspective transform
-        #Compute the inverse perspective transform:
-        Minv = cv2.getPerspectiveTransform(dst, src)
-        #Warp an image using the perspective transform, M:
-        warped = cv2.warpPerspective(img, M, img_size, flags=cv2.INTER_LINEAR)
+        # Perspective transform
 
-    #Camera calibration
+        # Warp an image using the perspective transform, M:
+        warped = cv2.warpPerspective(img, self.Minv, self.img_size, flags=cv2.INTER_LINEAR)
+
+    # find lane 
+    # params: processed image
+    def lane_search(self, image):
+        pass
+
+    def transform_perspective(self, image_path):
+
+        image = cv2.imread(image_path)
+        
+        src = np.float32([[1040, 680],[255, 680],[628, 431],[650, 431]])
+        dst = np.float32([[1040, 680],[255, 680],[255, 430],[1040, 430]])
+        # # Compute the inverse perspective transform:
+        self.Minv = cv2.getPerspectiveTransform(dst, src)
+        height, width, channel = image.shape
+        self.img_size = (height,width)
+        top_down = cv2.warpPerspective(image, self.Minv, self.img_size, flags=cv2.INTER_LINEAR)
+
+        if self.show_images:
+            f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+            f.tight_layout()
+            ax1.imshow(image)
+            # ax1.plt(src,'.')
+            ax1.set_title('Original Image', fontsize=50)
+            ax2.imshow(top_down)
+            # ax2.plt(src,'.')
+            ax2.set_title('Undistorted and Warped Image', fontsize=50)
+            plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+            plt.show()
+
+
+
+    # Camera calibration
+    # params: calibration images folder path 
     def calibrate(self, calibration_folder):
 
         objpoints = [] # 3d space points
         imagepoints = [] # 2d points in image plane
-
+        # list of files in directory
         image_names = os.listdir(calibration_folder)
-
+        # board inner corner size
         nx,ny = 9,6
 
         for image_name in image_names:
-            if image_name[-4:] == '.jpg':
+            if image_name[-4:] == '.jpg': # process images only
                 image_path = calibration_folder + '/' + image_name
                 image = cv2.imread(image_path)
                 # prepare points
-                objp = np.zeros((6*9,3), np.float32)
-                objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2) # x, y 
+                objp = np.zeros((nx*ny,3), np.float32)
+                objp[:,:2] = np.mgrid[0:nx,0:ny].T.reshape(-1,2) # x, y 
 
                 # find corners in 
                 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
                 #Finding chessboard corners (for an 8x6 board):
                 ret, corners = cv2.findChessboardCorners(gray, (nx,ny), None)
 
-                #Drawing detected corners on an image:
-                # image = cv2.drawChessboardCorners(image, (9,6), corners, ret)
-
                 if ret:
                     imagepoints.append(corners)
                     objpoints.append(objp)
 
         #Camera calibration, given object points, image points, and the shape of the grayscale image:
-        ret, self.mtx, self.dist, self.rvecs, self.tvecs = cv2.calibrateCamera(objpoints, imagepoints, gray.shape[::-1], None, None)
+        r, self.mtx, self.dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imagepoints, gray.shape[::-1], None, None)
 
         if self.show_images:
+            #Drawing detected corners on an image:
+            image = cv2.drawChessboardCorners(image, (nx,ny), corners, ret)
+            plt.imshow(image)
+            plt.show()
+
             # Plotting thresholded images
             image_original = cv2.imread(calibration_folder + '/' + image_names[10])
             f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20,10))
+            f.tight_layout()
             ax1.set_title('Original Image')
             ax1.imshow(image_original)
 
-            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            gray = cv2.cvtColor(image_original, cv2.COLOR_BGR2GRAY)
 
             #Finding chessboard corners (for an 8x6 board):
             ret, corners = cv2.findChessboardCorners(gray, (nx,ny), None)
 
-            dst = cv2.undistort(image, self.mtx, self.dist, None, self.mtx)
+            dst = cv2.undistort(image_original, self.mtx, self.dist, None, self.mtx)
 
             ax2.set_title('Undistorted Image')
             ax2.imshow(dst)
@@ -135,16 +180,17 @@ class lane_finding():
 
 LF = lane_finding(DEBUG)
 
-LF.calibrate('camera_cal')
-# white_output = 'test_videos_output/solidWhiteRight.mp4'
-# ## To speed up the testing process you may want to try your pipeline on a shorter subclip of the video
-# ## To do so add .subclip(start_second,end_second) to the end of the line below
-# ## Where start_second and end_second are integer values representing the start and end of the subclip
-# ## You may also uncomment the following line for a subclip of the first 5 seconds
-# ##clip1 = VideoFileClip("test_videos/solidWhiteRight.mp4").subclip(0,5)
-# clip1 = VideoFileClip("test_videos/solidWhiteRight.mp4")
-# white_clip = clip1.fl_image(process_image) #NOTE: this function expects color images!!
-# %time white_clip.write_videofile(white_output, audio=False)
+# LF.calibrate(CALIBRATION_FOLDER)
+LF.transform_perspective(TEST_FOLDER+'/'+'straight_lines1.jpg')
+
+if DEBUG:
+    # LF.process_image()
+    print ('Process here..')
+else:
+    white_output = 'test_videos_output/solidWhiteRight.mp4'
+    clip = VideoFileClip("test_videos/solidWhiteRight.mp4")
+    white_clip = clip1.fl_image(process_image)
+    white_clip.write_videofile(white_output, audio=False)
 
 
 
